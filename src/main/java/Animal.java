1c12b989b6050a86e5179eb7abf70f3d9b102342
danielsind <danielsindet@gmail.com>
2020-02-17 21:52:41 +0300

    Created properties on Abstracs class Animal.java -- added getter and setter methods too

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,2 +1,30 @@
-public class Animal {
+public abstract class Animal {
+    public String name;
+    public String health;
+    public String age;
+    public int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public void setHealth(String health) {
+        this.health = health;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
 }
